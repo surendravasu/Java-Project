29d91a751ff2e4d04812b62d14b478b95c0a09e3
surendravasu <vsvasu93@gmail.com>
2022-07-09 22:57:13 +0530

    Account Holder program

---
+++ Accountholder.java
@@ -0,0 +1,15 @@
+package bank.chennai;
+class Accountholder
+{
+	public static void main(String args[])
+	{
+		SBI account = new SBI();
+		account.get_loan(300000);
+		account.create_account();
+		System.out.println(SBI.branch_name);
+		
+	}
+}
+		
+		
+		
