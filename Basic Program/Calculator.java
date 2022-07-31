5a089e13b52208a332d7faac7ea5d4ef2a77cd29
surendravasu <vsvasu93@gmail.com>
2022-07-31 23:54:15 +0530

    Basic

---
+++ Basic Program/Calculator.java
@@ -0,0 +1,32 @@
+class Calculator
+{
+	public static void main(String args[])
+	{
+
+		Calculator calc= new Calculator();
+		double result = calc.add();
+		System.out.println(result);
+		calc.subtract(result,10);
+		char result2 = calc.check();
+		System.out.println(result2);
+		//byte result3 = calc.display();
+		System.out.println(calc.display());
+	}
+	void subtract(double no1,int no2)
+	{
+		System.out.println(no1-no2);
+	}
+	byte display()
+	{
+		return 125;
+	}
+	char check()
+	{
+		return 'v';
+
+	}
+	double add()//void empty artham
+	{
+		return 10.5+20;
+	}
+}
