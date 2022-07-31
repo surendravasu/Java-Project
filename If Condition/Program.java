2e6c954873bea60ad0f146e676f9829f495974c7
surendravasu <vsvasu93@gmail.com>
2022-07-31 23:36:19 +0530

    if condition program

---
+++ If Condition/Program.java
@@ -0,0 +1,27 @@
+
+public class Program 
+{ 
+
+	public static void main(String[] args) 
+	{
+		Program pro = new Program();
+		pro.prog();
+	}
+
+	public void prog() 
+	{
+		int b = 5; 
+		if (b == 5) 
+		{ 
+			System.out.println("TRUE");  
+		} 
+		else 
+		{
+			System.out.println("FALSE"); 
+		}
+		
+	}
+	
+}
+
+
